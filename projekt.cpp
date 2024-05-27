7d4a13cd526e327b67fa2ae65c93041b8b631778
AndrijaDean <andrija.dean@gmail.com>
2024-05-27 18:49:03 +0200

    Valjda popravljeni kod koji MOZDA radi
    Valjda popravljeni kod koji MOZDA radi

--- projekt.cpp
+++ projekt.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <algorithm>
 
+using namespace std;
+
 struct Igrac
 {
     char imeIgraca[50];
@@ -16,91 +18,107 @@ bool cmpp(Igrac &a, Igrac &b)
     return a.bodoviIgraca > b.bodoviIgraca;
 }
 
-using namespace std;
-
 int main()
 {
-    int izbor;
-    cout << "Unesite 1 ako želite vidjeti Leaderboard, unesite 2 ako želite zaigrati ili unesite 3 ako želite vidjeti shop.";
-    cin >> izbor;
-    if (izbor == 1)
+    struct Igrac igraci[100];
+    int brIgraca = 0;
+    while (1)
     {
+        cout << "Unesite ime igraca: ";
 
-        struct Igrac igraci[100];
-        int brIgraca = 0;
-        fstream leaderboard("leaderboard.bin", ios::binary | ios::in);
+        cin.ignore();
+        cin.getline(igraci[brIgraca + 1].imeIgraca, 50);
+        brIgraca++;
+        // cin >> igraci[brIgraca + i].bodoviIgraca;
 
-        while (leaderboard.read((char *)&igraci[brIgraca], sizeof(Igrac)))
+        int izbor;
+        cout << "Unesite 1 ako želite vidjeti Leaderboard, unesite 2 ako želite zaigrati ili unesite 3 ako želite vidjeti shop.";
+        cin >> izbor;
+        if (izbor == 1)
         {
-            cout << igraci[brIgraca].imeIgraca << " " << igraci[brIgraca].bodoviIgraca << endl;
-            brIgraca++;
-        }
-        leaderboard.close();
-        int unosIgraca;
-        cin >> unosIgraca;
-        for (int i = 0; i < unosIgraca; i++)
-        {
-            cin.ignore();
-            cin.getline(igraci[brIgraca + i].imeIgraca, 50);
-            cin >> igraci[brIgraca + i].bodoviIgraca;
-        }
-        sort(igraci, igraci + brIgraca + unosIgraca, cmpp);
-        fstream leaderboard1("leaderboard.bin", ios::binary | ios::out | ios::trunc);
-        leaderboard1.write((char*)igraci, sizeof(Igrac) * (brIgraca + unosIgraca));
-        leaderboard1.close();
-    }
-    else if (izbor == 2)
-    {
-        int n = 3;
-        int polje[n][n] = {
-            {0, 1, 2},
-            {3, 4, 5},
-            {6, 7, 8},
-        };
 
-        int rendom = time(0);
-        srand(rendom);
-        int bombaR = 0 + rand() % 2;
-        int bombaC = 0 + rand() % 2;
-        int bomba = polje[bombaR][bombaC];
-        // cout << bombaR << " "<< bombaC <<endl;
+            fstream leaderboard("leaderboard.bin", ios::binary | ios::in);
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
+            sort(igraci, igraci + brIgraca + 1, cmpp);
+
+            while (leaderboard.read((char *)&igraci[brIgraca], sizeof(Igrac)))
             {
-                cout << polje[i][j] << " ";
+                cout << igraci[brIgraca].imeIgraca << " " << igraci[brIgraca].bodoviIgraca << endl;
+                brIgraca++;
             }
-            cout << endl;
+            leaderboard.close();
+
+            fstream leaderboard1("leaderboard.bin", ios::binary | ios::out | ios::trunc);
+            leaderboard1.write((char *)igraci, sizeof(Igrac) * (brIgraca + 1));
+            leaderboard1.close();
         }
-
-        int pogadanjeR, pogadanjeC;
-        cout << "Unesite redak: ";
-        cin >> pogadanjeR;
-        cout << "Unesite stupac: ";
-        cin >> pogadanjeC;
-
-        int pogadanje = polje[pogadanjeR][pogadanjeC];
-
-        while (pogadanje != bomba)
+        else if (izbor == 2)
         {
-            cout << "Bomba nije pronađena!" << endl;
+            int n = 3;
+            int polje[n][n] = {
+                {0, 1, 2},
+                {3, 4, 5},
+                {6, 7, 8},
+            };
+
+            int rendom = time(0);
+            srand(rendom);
+            int bombaR = rand() % 3;
+            int bombaC = rand() % 3;
+            int bomba = polje[bombaR][bombaC];
+            cout << bombaR << " " << bombaC << endl;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    cout << polje[i][j] << " ";
+                }
+                cout << endl;
+            }
+
+            int pogadanjeR, pogadanjeC;
             cout << "Unesite redak: ";
             cin >> pogadanjeR;
             cout << "Unesite stupac: ";
             cin >> pogadanjeC;
 
-            pogadanje = polje[pogadanjeR][pogadanjeC];
+            int pogadanje = polje[pogadanjeR][pogadanjeC];
 
+            int brPogodenih = 0;
+            if (pogadanje != bomba)
+            {
+                brPogodenih++;
+            }
+
+            while (pogadanje != bomba)
+            {
+                cout << "Bomba nije pronađena!" << endl;
+                cout << "Unesite redak: ";
+                cin >> pogadanjeR;
+                cout << "Unesite stupac: ";
+                cin >> pogadanjeC;
+
+                pogadanje = polje[pogadanjeR][pogadanjeC];
+
+                if (pogadanje != bomba)
+                {
+                    brPogodenih++;
+                }
+                if (brPogodenih == n * n - 1)
+                {
+                    cout << "Dobili ste maksimalan broj bodova! :)" << " " << brPogodenih << endl;
+                    break;
+                }
+            }
             if (pogadanje == bomba)
             {
-                cout << "Bomba je pronađena!" << endl;
-                break;
+                cout << "Bomba je pronađena!" << " " << brPogodenih << endl;
             }
         }
-    }
-    else
-    {
+        else
+        {
+        }
     }
     return 0;
 }
